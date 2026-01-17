9319f8e85cf2ca33d3b2e65bb358ce566c927c29
faysel <fayselcode@gmail.com>
2026-01-17 20:42:45 +0300

    fix

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -68,9 +68,9 @@ export default function HeroSection() {
           </div>
 
           {/* Right visual - anchored bottom-right */}
-          <div className="lg:relative h-36 lg:h-full w-full lg:ltr:-mr-16  xl:ltr:-mr-32 top-0  lg:-top-4 lg:pb-0  max-lg:justify-center max-lg:items-center max-lg:flex max-lg:my-5">
+          <div className="lg:relative h-36 lg:h-full w-full top-0  lg:-top-10 lg:pb-0  max-lg:justify-center max-lg:items-center max-lg:flex max-lg:my-5">
             <div
-              className="lg:absolute lg:bottom-0 ltr:right-0 rtl:left-0 w-full max-w-40
+              className="lg:absolute lg:bottom-0 ltr:right-20 rtl:left-20 w-full max-w-40
              lg:max-w-88"
             >
               {/* Brain visualization */}
