9775a06dec7d800aca10503639c9b40293bb282d
Abubeker Abduljelil <Abubekera87@gmail.com>
2026-02-04 23:53:41 +0300

    feat: add og/twitter image

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,12 +47,21 @@ export async function generateMetadata(): Promise<Metadata> {
       description,
       locale: locale === "ar" ? "ar_AE" : "en_US",
       type: "website",
+      images: [
+        {
+          url: "/og-image.png",
+          width: 1200,
+          height: 630,
+          alt: "Astrah OS marketing image",
+        },
+      ],
     },
 
     twitter: {
       card: "summary_large_image",
       title,
       description,
+      images: ["/og-image.png"],
     },
   };
 }
